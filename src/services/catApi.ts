a226ba70590ccc98e6ac38b69fdd5d6f297609e4
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-07-01 11:22:18 -0300

    catApi

--- src/services/catApi.ts
+++ src/services/catApi.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
-export const getCats = async () => {
-   const response = await axios.get(
-      'https://api.thecatapi.com/v1/images/search?limit=10'
-   );
-   return response.data;
+export interface CatPost {
+  id: string;
+  url: string;
+}
+
+export const fetchCatPosts = async (limit: number): Promise<CatPost[]> => {
+  try {
+    const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${limit}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data from CatAPI:', error);
+    return [];
+  }
 };
